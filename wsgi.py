2f2526a431b0459cc2c20f06e00bcf971733280f
Vitor Menezes <vitor@menezeslog.com.br>
2025-06-07 13:46:06 -0400

    Corrige configuração do wsgi.py para Heroku

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,1 @@
-import sys
-import os
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
-from main_heroku_integrated import app as application
-
-if __name__ == "__main__":
-    app.run()
+from main_heroku_integrated import app
