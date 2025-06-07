b772fdd01d4bf7662f88ef7b6aaf03e91b513402
Vitor Menezes <vitor@menezeslog.com.br>
2025-06-07 09:07:51 -0400

    Corrige erro de importação para implantação no Heroku

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from src.main import app
+from main_heroku import app
 
 if __name__ == "__main__":
     app.run()
